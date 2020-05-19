3dc86d4af2f7f96dc82ab50c7f1fa3bf8c3890b9
kamil2022 <kamilowski40@wp.pl>
2020-05-19 11:04:00 +0200

    Zmiana w pliku main.cpp

--- 01/main.cpp
+++ 01/main.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
 #include <omp.h>
+#include <unistd.h>
 using namespace std;
 
 int main () {
     //omp_set_num_threads(6);
-    cout << omp_get_num_threads() << endl;
-    #pragma omp parallel 
+    //cout << omp_get_num_threads() << endl;
+    #pragma omp parallel num_threads(4)
     {
-        if (omp_get__thread_num() == 0) cout << omp_get_num_threads() << endl;
+        //if (omp_get__thread_num() == 0) cout << omp_get_num_threads() << endl;
         int ID = omp_get_thread_num();
         cout << "Watek nr " <<  ID  << endl;
     }
+    usleep(1000000);
 }
